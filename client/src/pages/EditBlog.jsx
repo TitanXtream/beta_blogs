7268a17eb7bf8376df3481cb67ed84043d1e14d9
titan <titanicking24@gmail.com>
2023-03-02 23:03:52 +0530

    CORRECTED:02/03

--- client/src/pages/EditBlog.jsx
+++ client/src/pages/EditBlog.jsx
@@ -5,6 +5,7 @@ import { updateBlog } from "../actions";
 import Form from "../components/Form";
 import { motion } from "framer-motion";
 import { staggerContainer, slideIn } from "../utils/motion";
+import { toast } from "react-toastify";
 
 const EditBlog = () => {
   const { id } = useParams();
@@ -12,10 +13,12 @@ const EditBlog = () => {
   const dispatch = useDispatch();
 
   const handleBlogEditing = async (editedBlog) => {
-    await dispatch(updateBlog(editedBlog));
-    setTimeout(() => {
-      navigate("/");
-    }, 500);
+    const resp = dispatch(updateBlog(editedBlog))
+      .then(() => navigate("/"))
+      .catch((err) => toast.error("Could not update"));
+    toast.promise(resp, {
+      pending: "Updating your blog",
+    });
   };
   return (
     <motion.section
